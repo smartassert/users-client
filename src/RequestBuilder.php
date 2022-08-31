d9dba87191d07effddb6b8b7d267037feca9c8f9
Jon Cram <webignition@gmail.com>
2022-08-31 11:20:06 +0100

    Simplify RequestBuilder

--- src/RequestBuilder.php
+++ src/RequestBuilder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace SmartAssert\UsersClient;
 
-use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 
 class RequestBuilder
@@ -12,43 +11,17 @@ class RequestBuilder
     public const DEFAULT_AUTHORIZATION_HEADER_NAME = 'Authorization';
     public const DEFAULT_AUTHORIZATION_VALUE_PREFIX = 'Bearer ';
 
-    private RequestInterface $request;
-
     public function __construct(
-        private readonly RequestFactoryInterface $requestFactory,
         private readonly string $authorizationHeaderName = self::DEFAULT_AUTHORIZATION_HEADER_NAME,
         private readonly string $authorizationValuePrefix = self::DEFAULT_AUTHORIZATION_VALUE_PREFIX,
     ) {
     }
 
-    public function create(string $method, string $url): self
+    public function addJwtAuthorizationHeader(RequestInterface $request, string $token): RequestInterface
     {
-        $this->request = $this->requestFactory->createRequest($method, $url);
-
-        return $this;
-    }
-
-    public function createVerifyApiTokenRequest(string $url, string $token): RequestInterface
-    {
-        return $this
-            ->create('GET', $url)
-            ->addJwtAuthorizationHeader($token)
-            ->getRequest()
-        ;
-    }
-
-    public function addJwtAuthorizationHeader(string $token): self
-    {
-        $this->request = $this->request->withHeader(
+        return $request->withHeader(
             $this->authorizationHeaderName,
             $this->authorizationValuePrefix . $token
         );
-
-        return $this;
-    }
-
-    public function getRequest(): RequestInterface
-    {
-        return $this->request;
     }
 }
