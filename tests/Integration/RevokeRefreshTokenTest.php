f5fbc9963d8ec974aa76b7d2096cdf1a0407a78b
Jon Cram <webignition@gmail.com>
2022-09-07 11:27:16 +0100

    Test revoke refresh token (#84)

---
+++ tests/Integration/RevokeRefreshTokenTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\UsersClient\Tests\Integration;
+
+use SmartAssert\UsersClient\Model\RefreshableToken;
+use SmartAssert\UsersClient\Model\User;
+
+class RevokeRefreshTokenTest extends AbstractIntegrationTest
+{
+    public function testCreateVerifyRefreshFrontendToken(): void
+    {
+        $token = $this->client->createFrontendToken(self::USER_EMAIL, self::USER_PASSWORD);
+        \assert($token instanceof RefreshableToken);
+
+        $userFromToken = $this->client->verifyFrontendToken($token);
+        \assert($userFromToken instanceof User);
+
+        $refreshedToken = $this->client->refreshFrontendToken($token);
+        \assert($refreshedToken instanceof RefreshableToken);
+
+        $this->client->revokeFrontendRefreshToken(self::ADMIN_TOKEN, $userFromToken->id);
+
+        self::assertNull($this->client->refreshFrontendToken($refreshedToken));
+    }
+}
