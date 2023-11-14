acd6b1e87fc0e0f19b16b9c649fa4af6880af447
Jon Cram <webignition@gmail.com>
2023-11-14 14:47:35 +0000

    Implement on Token

--- src/Model/Token.php
+++ src/Model/Token.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace SmartAssert\UsersClient\Model;
 
-class Token
+use SmartAssert\ServiceClient\SerializableInterface;
+
+readonly class Token implements SerializableInterface
 {
     /**
      * @param non-empty-string $token
      */
     public function __construct(
-        public readonly string $token,
+        readonly string $token,
     ) {
+    }
+
+    /**
+     * @return array{token: non-empty-string}
+     */
+    public function toArray(): array
+    {
+        return [
+            'token' => $this->token,
+        ];
     }
 }
